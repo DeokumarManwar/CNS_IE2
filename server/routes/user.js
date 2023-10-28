3e1976e89bcb1f8b755974ab7ec781b759219aa2
Deokumar Manwar <devmanwar305@gmail.com>
2023-10-28 23:21:03 +0530

    Added Password Security

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,20 +1,36 @@
 const { Router, response } = require("express");
 const router = require("express").Router();
 const user = require("../models/user");
+const crypto = require("crypto");
 
 router.get("/login", async (req, res) => {
   try {
-    const data = await user.findOne({
-      userName: req.headers.username,
-      password: req.headers.password,
-    });
-    if (data) {
-      return res.status(200).send({ success: true, user: data });
-    } else {
-      return res.status(400).send({ success: false, msg: "Data Not Found" });
+    const username = req.headers.username;
+    const providedPassword = req.headers.password;
+
+    const userRecord = await user.findOne({ userName: username });
+
+    if (userRecord) {
+      // SHA-1 hashing for the provided password
+      const sha1Hash = crypto
+        .createHash("sha1")
+        .update(providedPassword)
+        .digest("hex");
+
+      // Decipher the transposed hash and compare it to the stored hash
+      const transpositionKey = process.env.key; // Replace with your key
+      const storedTransposedHash = userRecord.password;
+      const decipheredHash = transpose(sha1Hash, transpositionKey);
+
+      console.log(decipheredHash, storedTransposedHash);
+      if (decipheredHash === storedTransposedHash) {
+        return res.status(200).send({ success: true, user: userRecord });
+      }
     }
+
+    return res.status(400).send({ success: false, msg: "Login Failed" });
   } catch (e) {
-    return res.status(505).json({ message: e });
+    return res.status(500).json({ message: e });
   }
 });
 
@@ -51,23 +67,48 @@ router.get("/getOne/:id", async (req, res) => {
 });
 
 router.post("/signup", async (req, res) => {
-  const data = await user.findOne({ userName: req.body.userName });
-  if (!data) {
-    const newUser = user({
-      userName: req.body.userName,
-      password: req.body.password,
-      image_url: req.body.image_url,
-    });
-    try {
-      const savedUser = await newUser.save();
-      return res.status(200).send({ success: true, user: savedUser });
-    } catch (e) {
-      return res.status(505).json({ message: "Cannot Create User" });
-    }
-  } else {
-    return res.status(505).json({ message: "User Already Exists" });
+  const { userName, password, image_url } = req.body;
+  const existingUser = await user.findOne({ userName: userName });
+
+  if (existingUser) {
+    return res.status(409).json({ message: "User Already Exists" });
   }
+
+  // SHA-1 hashing
+  const sha1Hash = crypto.createHash("sha1").update(password).digest("hex");
+
+  // Columnar Transposition Cipher
+  const transpositionKey = process.env.key; // Replace with your key
+  const transposedHash = transpose(sha1Hash, transpositionKey);
+
+  console.log(transposedHash);
+  const newUser = {
+    userName,
+    password: transposedHash,
+    image_url,
+  };
+
+  const savedUser = await user.create(newUser);
+
+  return res.status(200).send({ success: true, user: savedUser });
 });
+
+// Function to perform columnar transposition
+function transpose(input, key) {
+  let output = "";
+  const keyLength = key.length;
+  const inputLength = input.length;
+
+  for (let i = 0; i < keyLength; i++) {
+    let index = i;
+    while (index < inputLength) {
+      output += input[index];
+      index += keyLength;
+    }
+  }
+
+  return output;
+}
 
 router.get("/getAll", async (req, res) => {
   try {
